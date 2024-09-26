fbf0dc5a0cba65f6deda4c026232400d701e1ee3
kirstinlyon <kirstin@mltwelve.com>
2024-09-26 19:05:07 +0200

    Added abbreviations

--- Scripts/pepfar_available_funds_data.R
+++ Scripts/pepfar_available_funds_data.R
@@ -2,6 +2,24 @@ library(tidyverse)
 library(blingr)
 library(janitor)
 
+
+
+#Abbreviations--------------------------------------------------------
+# Create a named vector for lookup
+program_element_map <- c(
+    "HL.1.11" = "HTXS",
+    "HL.1.13" = "OHSS",
+    "HL.1.17" = "PDCS",
+    "HL.1.8"  = "HKID",
+    "HL.1.2"  = "HVAB",
+    "HL.1.5"  = "HVOP",
+    "HL.1.9"  = "HVCT",
+    "HL.1.14" = "HIS",
+    "HL.1.15" = "CIRC",
+    "HL.1.19" = "HVMS"
+)
+
+
 #COMBINE ALL MONTHS.  After data has been udpated by team ------------------------------------------------------
 #1. OBLIGATIONS AND ACCRUALS-----------------------------------------
 DOAG_DATE <- readxl::read_xlsx("Data/DOAG Amendment Obligation Dates.xlsx") |> 
@@ -26,7 +44,8 @@ write_csv(pepfar_all_bi_acc_lines_mech, "Dataout/pepfar_all_bi_oblg_acc_lines_me
 pepfar_all_bi_acc_lines_no_mech <- map(bi_acc_lines_all_files, ~ blingr::create_bi_oblg_acc_lines_updated_no_mech(.x, TRUE)) |> 
     bind_rows() |> 
     select(1:24) |> 
-    left_join(DOAG_DATE, by = c('Document Number' = "document_number"))
+    left_join(DOAG_DATE, by = c('Document Number' = "document_number")) |> 
+    mutate(abbrev = recode(`Program Element`, !!!program_element_map))
 
 write_csv(pepfar_all_bi_acc_lines_no_mech, "Dataout/pepfar_all_bi_oblg_acc_lines_no_mech.csv")
 
@@ -41,7 +60,8 @@ open_commitments_all_files <- dir(OPEN_COMMITMENTS_HISTORY_PATH,
 
 #combine all datasets into one and add period based on file name
 pepfar_all_open_commitments <- map(open_commitments_all_files, blingr::create_history_open_commitments)  |> 
-    bind_rows()
+    bind_rows() |> 
+    mutate(abbrev = recode(program_element, !!!program_element_map))
 
 #write data
 write_csv(pepfar_all_open_commitments, "Dataout/pepfar_all_open_commitments.csv")
