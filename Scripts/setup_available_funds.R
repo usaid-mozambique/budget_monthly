6fc7a6219e712a839af3047ae431b327cfa709f0
kirstinlyon <kirstin@mltwelve.com>
2024-09-26 21:54:45 +0200

    set-up folder for available funds. Create all folders and install libraries before running available funds.

---
+++ Scripts/setup_available_funds.R
@@ -0,0 +1,29 @@
+required_packages <- c("tidyverse", "janitor", "glamr","blingr")
+
+missing_packages <- required_packages[!(required_packages %in% installed.packages()
+                                        [,"Package"])]
+
+if(length(missing_packages) >0){
+    install.packages(missing_packages, repos = c("https://usaid-oha-si.r-universe.dev",
+                                                 "https://cloud.r-project.org"))
+}
+
+library(glamr)
+library(tidyverse)
+library(janitor)
+library(blingr)
+
+
+# OTHER SETUP  - only run one-time --------------------------------------
+
+folder_setup() 
+folder_setup(folder_list = list("Data/bi_acc_lines/raw/",   #bilateral accounting lines raw data here - one file at a time
+                                "Data/open_commitment/raw/", #open commitment raw data - one file at a time
+                                "Data/bi_acc_lines/processed/non_pepfar", #processed data for non-pepfar
+                                "Data/open_commitment/processed/non_pepfar", #processed data for non-pepfar
+                                "Data/bi_acc_lines/processed/pepfar/", #processed data for pepfar
+                                "Data/open_commitment/processed/pepfar/"
+                                )
+             )
+
+
